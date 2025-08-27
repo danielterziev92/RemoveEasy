89f3c7c9daa58bdf336ceddd32390be00da674fa
Daniel Terziev <daniel.st.terziev@gmail.com>
2025-08-27 12:51:05 +0300

    Initial Django project setup with `removeeasy`:
    - Added `.gitignore` file for Python, Django, and IDE-specific exclusions.
    - Created Django project structure with `removeeasy`:
      - `settings.py`, `urls.py`, `wsgi.py`, `asgi.py`
      - Administrative script: `manage.py`
    - Included `pyproject.toml` with project dependencies:
      - Django, CORS headers, `django-environ`, `django-ninja`, and `django-ninja-extra`.
    - Configured settings for apps, middleware, templates, database, and static/media files.

---
+++ removeeasy/settings.py
@@ -0,0 +1,127 @@
+import os
+import stat
+from datetime import timedelta
+from pathlib import Path
+
+from environ import Env
+
+# Path Settings
+BASE_DIR = Path(__file__).resolve().parent.parent
+env = Env()
+env.read_env(os.path.join(BASE_DIR, ".env"))
+
+# Core Django Settings
+SECRET_KEY = env.str("SECRET_KEY")
+DEBUG = env.bool("DEBUG")
+ROOT_URLCONF = "removeeasy.urls"
+WSGI_APPLICATION = "removeeasy.wsgi.application"
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# Security Settings
+CSRF_COOKIE_HTTPONLY = True
+SESSION_COOKIE_HTTPONLY = True
+
+# Host Settings
+ALLOWED_HOSTS = env.list("ALLOWED_HOSTS")
+CSRF_TRUSTED_ORIGINS = env.list("CORS_ALLOWED_ORIGINS")
+
+# CORS Settings
+CORS_ALLOWED_ORIGINS = env.list("CORS_ALLOWED_ORIGINS")
+CORS_ALLOW_METHODS = env.list("CORS_ALLOW_METHODS")
+CORS_ALLOW_ALL_ORIGINS = env.bool("CORS_ALLOW_ALL_ORIGINS")
+CORS_ALLOW_CREDENTIALS = env.bool("CORS_ALLOW_CREDENTIALS")
+CORS_EXPOSE_HEADERS = env.list("CORS_EXPOSE_HEADERS")
+CORS_ALLOW_HEADERS = env.list("CORS_ALLOW_HEADERS")
+
+# Application Definition
+DJANGO_APPS = (
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+)
+
+PROJECT_APPS = (
+)
+
+THIRD_PARTY_APPS = (
+    "corsheaders",
+    "ninja",
+    "ninja_extra",
+)
+
+INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + PROJECT_APPS
+
+# Middleware Configuration
+MIDDLEWARE = (
+    "corsheaders.middleware.CorsMiddleware",
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
+)
+
+# Template Configuration
+TEMPLATES = [
+    {
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [BASE_DIR / "templates"],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
+            ],
+        },
+    },
+]
+
+# Database
+# https://docs.djangoproject.com/en/5.2/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
+# Authentication Settings
+
+AUTH_PASSWORD_VALIDATORS = (
+    {"NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator", },
+    {"NAME": "django.contrib.auth.password_validation.MinimumLengthValidator", },
+    {"NAME": "django.contrib.auth.password_validation.CommonPasswordValidator", },
+    {"NAME": "django.contrib.auth.password_validation.NumericPasswordValidator", },
+)
+
+# Internationalization Settings
+LANGUAGE_CODE = "en-us"
+TIME_ZONE = "UTC"
+USE_I18N = False
+USE_TZ = True
+
+# Static Files Settings
+STATIC_URL = "/static/"
+STATIC_ROOT = "/staticfiles"
+STATICFILES_DIRS = []
+STATICFILES_FINDERS = (
+    "django.contrib.staticfiles.finders.FileSystemFinder",
+    "django.contrib.staticfiles.finders.AppDirectoriesFinder",
+)
+
+# Media files
+MEDIA_URL = "/media/"
+MEDIA_ROOT = BASE_DIR / "mediafiles"
+if not MEDIA_ROOT.exists():
+    MEDIA_ROOT.mkdir(parents=True, exist_ok=True)
+
+# Default primary key field type
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
