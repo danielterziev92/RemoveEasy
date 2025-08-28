7ebed16ff7cce31d9933fd32533e3f4e94284506
Daniel Terziev <daniel.st.terziev@gmail.com>
2025-08-28 09:21:32 +0300

    Add `InventoryItem` model and update `InventorySection` with `icon_class`
    - Added `InventoryItem` model for managing inventory items linked to sections.
    - Updated `InventorySection` model to include `icon_class` field.
    - Created migrations for `InventoryItem` and updated `InventorySection` changes.
    - Defined related name `items`

--- apps/inventory/models.py
+++ apps/inventory/models.py
@@ -2,7 +2,13 @@ from django.db import models
 
 
 class InventorySection(models.Model):
+    ICON_CLASS_MAX_LENGTH = 100
     TITLE_MAX_LENGTH = 100
+
+    icon_class = models.CharField(
+        max_length=ICON_CLASS_MAX_LENGTH,
+        null=False
+    )
 
     title = models.CharField(
         max_length=TITLE_MAX_LENGTH,
@@ -16,3 +22,32 @@ class InventorySection(models.Model):
         db_table = "inventory_sections"
         verbose_name = "Инвентар Секция"
         verbose_name_plural = "Инвентар Секции"
+
+
+class InventoryItem(models.Model):
+    ICON_CLASS_MAX_LENGTH = 100
+    TITLE_MAX_LENGTH = 100
+
+    icon_class = models.CharField(
+        max_length=ICON_CLASS_MAX_LENGTH,
+        null=False
+    )
+
+    title = models.CharField(
+        max_length=TITLE_MAX_LENGTH,
+        null=False,
+    )
+
+    section = models.ForeignKey(
+        InventorySection,
+        on_delete=models.CASCADE,
+        related_name="items",
+    )
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        db_table = "inventory_items"
+        verbose_name = "Инвентар Предмет"
+        verbose_name_plural = "Инвентар Предмети"
