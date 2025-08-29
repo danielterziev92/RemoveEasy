8e925e70c53c512b10cbf8efea1a4f2bccd59499
Daniel Terziev <daniel.st.terziev@gmail.com>
2025-08-29 22:57:33 +0300

    Add `inventorySlice` for Redux Toolkit state management
    - Implemented `inventorySlice` with actions and reducers to manage inventory state.
    - Included selectors for accessing sections, items, loading, and error states.
    - Defined initial state for inventory with `sections`, `items`, `isLoading`, and `error` properties.

---
+++ client/src/application/store/slices/inventorySlice.ts
@@ -0,0 +1,52 @@
+import type {Section} from "../../../domain/entities/Section";
+import type {Item} from "../../../domain/entities/Item.ts";
+import {createSlice, type PayloadAction} from "@reduxjs/toolkit";
+
+interface InventoryState {
+    sections: Section[];
+    items: Item[];
+    isLoading: boolean;
+    error: string | null;
+}
+
+const initialState: InventoryState = {
+    sections: [],
+    items: [],
+    isLoading: false,
+    error: null,
+};
+
+const inventorySlice = createSlice({
+    name: "inventory",
+    initialState,
+    reducers: {
+        setLoading: (state, action: PayloadAction<boolean>) => {
+            state.isLoading = action.payload;
+        },
+        setError: (state, action: PayloadAction<string | null>) => {
+            state.error = action.payload;
+            state.isLoading = false;
+        },
+        setInventoryData: (state, action: PayloadAction<{ sections: Section[]; items: Item[] }>) => {
+            state.sections = action.payload.sections;
+            state.items = action.payload.items;
+            state.isLoading = false;
+            state.error = null;
+        },
+        clearInventoryData: (state) => {
+            state.sections = [];
+            state.items = [];
+            state.error = null;
+            state.isLoading = false;
+        },
+    },
+});
+
+export const {setLoading, setError, setInventoryData, clearInventoryData} = inventorySlice.actions;
+
+export const selectSections = (state: { inventory: InventoryState }) => state.inventory.sections;
+export const selectItems = (state: { inventory: InventoryState }) => state.inventory.items;
+export const selectIsLoading = (state: { inventory: InventoryState }) => state.inventory.isLoading;
+export const selectError = (state: { inventory: InventoryState }) => state.inventory.error;
+
+export default inventorySlice.reducer;
