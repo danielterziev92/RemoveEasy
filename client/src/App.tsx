361bceaf80689b1e1e0eb37fffb87442e0847a16
Daniel Terziev <daniel.st.terziev@gmail.com>
2025-09-01 01:17:00 +0300

    Add `NotFound` page and configure fallback route
    - Implemented `NotFound` page with responsive design and fallback content.
    - Updated `App` component to include a wildcard route for undefined paths.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,9 @@
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 
-import Index from "@/presentation/pages/Index.tsx";
 import {useLocalizationInit} from "@/hooks/useLocalizationInit.ts";
+
+import Index from "@/presentation/pages/Index.tsx";
+import NotFound from "@/presentation/pages/NotFound.tsx";
 
 export default function App() {
     useLocalizationInit();
@@ -10,6 +12,8 @@ export default function App() {
         <BrowserRouter>
             <Routes>
                 <Route path={"/"} element={<Index/>}/>
+
+                <Route path="*" element={<NotFound/>}/>
             </Routes>
         </BrowserRouter>
     )
